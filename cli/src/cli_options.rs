88668432f3d0aeb3e0d2b06039a43f989f3a18ab
Pavel Bezglasnyy <pavelb23@gmail.com>
2025-10-20 14:19:43 +0200

    Update dockerfile: remove explicit cache layers

--- cli/src/cli_options.rs
+++ cli/src/cli_options.rs
@@ -1,9 +1,20 @@
+use clap::ArgAction;
 use clap::{Parser, Subcommand, ValueEnum};
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 pub enum ServeType {
     Http,
     Grpc,
+}
+
+impl Display for ServeType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ServeType::Http => write!(f, "http"),
+            ServeType::Grpc => write!(f, "grpc"),
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -32,7 +43,7 @@ pub enum CliCommand {
     /// Start a feature server locally on a given port
     Serve {
         /// Specify a host for the server
-        #[arg(short = 'n', long = "host", default_value = "127.0.0.1")]
+        #[arg(short = 'h', long = "host", default_value = "127.0.0.1")]
         host: String,
         /// Specify a port for the server
         #[arg(short = 'p', long = "port", default_value_t = 6566)]
@@ -56,12 +67,15 @@ pub enum CliCommand {
 }
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about, long_about = None, disable_help_flag = true, name = "feast-server-rust")]
 pub struct CliOptions {
     #[arg(short = 'c', long = "chdir", default_value = None)]
     /// Switch to a different feature repository directory before executing the given subcommand.
     /// Can also be set via the FEATURE_REPO_DIR_ENV_VAR environment variable.
     pub chdir: Option<String>,
+    /// Print help information
+    #[arg(long = "help", action = ArgAction::Help, help = "Print help information")]
+    pub help: Option<bool>,
     /// The logging level. Case-insensitive.
     #[arg(
         value_enum,
