232a59af434a6429b6e86457797f65b79fa549e1
Pavel Bezglasnyy <pavelb23@gmail.com>
2025-10-29 23:02:32 +0100

    added grpc bench

---
+++ cli/build.rs
@@ -0,0 +1,15 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(false)
+        .compile_protos(
+            &["../feast-server-core/protos/feast/serving/ServingService.proto"],
+            &["../feast-server-core/protos"],
+        )?;
+
+    println!(
+        "cargo:rerun-if-changed=../feast-server-core/protos/feast/serving/ServingService.proto"
+    );
+    println!("cargo:rerun-if-changed=../feast-server-core/protos/feast/types/Value.proto");
+
+    Ok(())
+}
